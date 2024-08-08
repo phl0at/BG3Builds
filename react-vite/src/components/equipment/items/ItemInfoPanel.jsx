8235a54dff989d045811f83176d342d66eb2aa30
Max Fonda <mxwll155@gmail.com>
2024-08-08 14:44:30 -0400

    modified items list component

---
+++ react-vite/src/components/equipment/items/ItemInfoPanel.jsx
@@ -0,0 +1,80 @@
+// Files
+import styles from "./Items.module.css";
+// Functions/Components
+import { action, equipItem } from "../../../redux/build";
+// Packages
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+export default function ItemInfo({ wearingItem, selectedItem, type }) {
+  const dispatch = useDispatch();
+  const { buildId } = useParams();
+  const Equipment = useSelector((state) => state.static.equipment);
+  const currentUser = useSelector((state) => state.session.user);
+  const viewedItem = Equipment[selectedItem];
+  const descriptions = viewedItem?.description.split("&*&");
+
+  if(!viewedItem) return <main className={styles.hiddenMenu}></main>
+
+  return (
+    <main className={styles.sideMenu}>
+      <div className={styles.sideTable}>
+        <div className={styles.sideInfo}>
+          <div className={styles.desHead}>
+            <div>{viewedItem.name}</div>
+          </div>
+          <div className={styles.rarity}>{viewedItem.rarity}</div>
+          {viewedItem.damage_type != null && (
+            <div className={styles.damage_type}>
+              {viewedItem.damage_type} weapon
+            </div>
+          )}
+          {viewedItem.damage != null && (
+            <div className={styles.damage}>Damage | {viewedItem.damage}</div>
+          )}
+        </div>
+        <div className={styles.description}>
+          {descriptions.map((des) => {
+            const title = des.split(":")[0];
+            const text = des.split(":")[1];
+            return (
+              <div key={title}>
+                <div className={styles.orange}>{title}</div>
+                <div className={styles.text}>{text}</div>
+                <br />
+              </div>
+            );
+          })}
+        </div>
+        {!currentUser & (buildId != undefined) ? (
+          <div className={styles.buttons}>Sign in to change Equipment</div>
+        ) : (
+          <div className={styles.buttons}>
+            {!wearingItem && (
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(equipItem(type, selectedItem));
+                }}
+                className={styles.equip}
+              >
+                Equip
+              </button>
+            )}
+            {wearingItem && (
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(action("build/remove", type));
+                }}
+                className={styles.equip}
+              >
+                Remove
+              </button>
+            )}
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
--- react-vite/src/components/equipment/items/Items.jsx
+++ react-vite/src/components/equipment/items/Items.jsx
@@ -1,26 +1,20 @@
 //Files
 import styles from "./Items.module.css";
-//Functions/Components
-import { equipItem, action } from "../../../redux/build";
 //Packages
-import { useParams } from "react-router-dom";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { IKImage } from "imagekitio-react";
+import { PulseLoader } from "react-spinners";
+import { useState, lazy, Suspense } from "react";
+//Functions/Components
+const ItemInfo = lazy(() => import("./ItemInfoPanel"));
 
 export default function ItemsTableModal({ type }) {
-  const { buildId } = useParams();
-  const dispatch = useDispatch();
   const currentBuild = useSelector((state) => state.builds.current);
-  const currentUser = useSelector((state) => state.session.user);
   const Equipment = useSelector((state) => state.static.equipment);
-  const [selectedItem, setSelectedItem] = useState(
-    currentBuild[type] ? currentBuild[type] : null
-  );
+  const [selectedItem, setSelectedItem] = useState(currentBuild[type]);
+  const wearingItem = currentBuild[type] === selectedItem;
   const formatType = type.split("_")[0];
   const capitalizedType = formatType[0].toUpperCase() + formatType.slice(1);
-  const viewedItem = Equipment[selectedItem];
-  const descriptions = viewedItem?.description.split("&*&");
 
   const onClick = (e, id) => {
     e.preventDefault();
@@ -28,8 +22,8 @@ export default function ItemsTableModal({ type }) {
   };
 
   return (
-    <>
-      <main className={styles.main}>
+    <div className={styles.itemsModal}>
+      <main className={styles.itemsList}>
         <div className={styles.table}>
           <div className={styles.thead}>
             <div className={styles.itemHead}>{`${capitalizedType}`}</div>
@@ -61,64 +55,19 @@ export default function ItemsTableModal({ type }) {
           })}
         </div>
       </main>
-      {selectedItem && (
-        <main className={styles.sideMenu}>
-          <div className={styles.sideTable}>
-            <div className={styles.sideInfo}>
-              <div className={styles.desHead}>
-                <div>{viewedItem.name}</div>
-              </div>
-              <div className={styles.rarity}>{viewedItem.rarity}</div>
-              {viewedItem.damage_type != null && (
-                <div className={styles.damage_type}>
-                  {viewedItem.damage_type} weapon
-                </div>
-              )}
-              {viewedItem.damage != null && (
-                <div className={styles.damage}>
-                  Damage | {viewedItem.damage}
-                </div>
-              )}
-            </div>
-            {descriptions.length > 1 ? (
-              <>
-                <div className={styles.description}>{descriptions[0]}</div>
-                <div className={styles.description}>{descriptions[1]}</div>
-              </>
-            ) : (
-              <div className={styles.description}>{descriptions[0]}</div>
-            )}
-            {!currentUser & (buildId != undefined) ? (
-              <div className={styles.buttons}>Sign in to change Equipment</div>
-            ) : (
-              <div className={styles.buttons}>
-                {currentBuild[type] !== selectedItem && (
-                  <button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      dispatch(equipItem(type, selectedItem));
-                    }}
-                    className={styles.equip}
-                  >
-                    Equip
-                  </button>
-                )}
-                {currentBuild[type] === selectedItem && (
-                  <button
-                    onClick={(e) => {
-                      e.preventDefault();
-                      dispatch(action("build/remove", type));
-                    }}
-                    className={styles.equip}
-                  >
-                    Remove
-                  </button>
-                )}
-              </div>
-            )}
-          </div>
-        </main>
-      )}
-    </>
+      <Suspense
+        fallback={
+          <main className={styles.sideMenu}>
+            <PulseLoader />
+          </main>
+        }
+      >
+        <ItemInfo
+          wearingItem={wearingItem}
+          selectedItem={selectedItem}
+          type={type}
+        />
+      </Suspense>
+    </div>
   );
 }
