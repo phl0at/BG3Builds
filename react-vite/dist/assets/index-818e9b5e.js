a0bae9ceaf4bb27b4b0cba96723a3ab3805a53f4
Max Fonda <mxwll155@gmail.com>
2024-08-16 17:14:10 -0400

    improved subclass component styling

---
+++ react-vite/dist/assets/index-818e9b5e.js
@@ -0,0 +1,1 @@
+import{O as t}from"./OpenModalButton-5f93c555.js";import"./index-692dd1c2.js";export{t as default};
--- react-vite/dist/assets/index-8a082c68.js
+++
@@ -1,1 +0,0 @@
-import{O as t}from"./OpenModalButton-53110ae2.js";import"./index-52daad0c.js";export{t as default};
