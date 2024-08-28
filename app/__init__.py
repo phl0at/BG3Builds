b092977f8cf47bfa22fbf3864e6da426332be6ee
Max Fonda <mxwll155@gmail.com>
2024-08-28 14:31:22 -0400

    https enabled in flask

--- app/__init__.py
+++ app/__init__.py
@@ -99,3 +99,8 @@ def react_root(path):
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    context = ('local.crt', 'local.key')#certificate and key files
+    app.run(debug=True, ssl_context=context)
