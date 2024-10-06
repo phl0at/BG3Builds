85b69f4aeb485c492573c5dcb10e78d1515cb03e
Max Fonda <mxwll155@gmail.com>
2024-10-05 20:10:08 -0400

    added key values for different cantrip types

--- react-vite/dist/assets/index-33efcdad.js
+++
@@ -1,1 +0,0 @@
-import{O as t}from"./OpenModalButton-8e0030f6.js";import"./index-15ed3a41.js";export{t as default};
---
+++ react-vite/dist/assets/index-da3beaaa.js
@@ -0,0 +1,1 @@
+import{O as t}from"./OpenModalButton-265efc6e.js";import"./index-6f3e6fb1.js";export{t as default};
